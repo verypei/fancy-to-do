3208b5884ce4039500cd19334f41d308c4364427
verypei <verrymariyanto@gmail.com>
2020-03-31 12:16:50 +0700

    API added

--- index.js
+++ index.js
@@ -1,14 +1,16 @@
 const express = require("express");
 const app = express();
 const port = 3000;
-const todoRouter = require("./routers/todoRouter")
-const userRouter = require("./routers/userRouter")
+const todoRouter = require("./routers/todoRouter");
+const userRouter = require("./routers/userRouter");
+const weatherRouter = require("./routers/APIrouter");
 
 app.use(express.urlencoded({extended:false}));//express method that to recognize the incoming request object as string or arrays
 
 //middleware
 app.use("/todos",todoRouter);
 app.use("/users",userRouter);
+app.use("/weather",weatherRouter);
 app.use(express.json())//express method that to recognize the incoming request object as JSON object
 
 app.listen(port,()=>{
---
+++ routers/APIrouter.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const router = express.Router();
+const axios = require("axios");
+
+router.get("/",(req,res)=>{
+    console.log("masuk  pa eko")
+    axios.get('http://api.openweathermap.org/data/2.5/weather?q=London,uk&appid=3736aac5d5b0505bf92dc61ce8b220a4')
+    .then(function(response){
+        // handle success
+        res.status(200).json(response.data)
+        // console.log(response);
+    })
+    .catch(function(error){
+        // handle error
+        res.status(404).json(error)
+        // console.log(error);
+    })
+})
+
+module.exports=router
